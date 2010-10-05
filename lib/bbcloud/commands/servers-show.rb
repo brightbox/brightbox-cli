dbfd8b2112a7e2d3fb66a931e8e396ec36579c0b
John Leach <john@johnleach.co.uk>
2010-10-05 18:33:50 +0100

    servers-show: New vertical table output format

--- lib/bbcloud/commands/servers-show.rb
+++ lib/bbcloud/commands/servers-show.rb
@@ -3,37 +3,44 @@ arg_name 'server-id...'
 command [:show] do |c|
 
   c.action do |global_options,options,args|
-
     servers = args.collect { |arg| Api.conn.servers.get(arg) || arg }
-    
+    rows = []
     servers.each do |s|
       if s.is_a? String
         error "Could not find server #{s}"
         next
       end
-      field "id", s.id
-      field "name", s.name
-      field "description", s.description
-      field "status", s.status
+      o = s.attributes
+      o[:zone] = s.zone_id
+      if zone = Api.conn.zones.get(s.zone_id)
+        o[:zone] = zone.name
+      end
+      o[:type] = s.flavor_id
       if type = Api.conn.flavors.get(s.flavor_id)
-        field "type", "#{type.id} (#{type.name})"
-        field "ram", type.ram
-        field "cores", type.cores
-        field "disk", type.disk.to_i
-      else
-        field "type", s.flavor_id
+        o[:type_name] = type.name
+        o[:ram] = type.ram
+        o[:cores] = type.cores
+        o[:disk] = type.disk.to_i
       end
+      o[:image] = s.image_id
       if image = Api.conn.images.get(s.image_id)
-        field "image", "#{image.id} (#{image.name})"
-        field "arch", image.arch
-      else
-        field "image", s.image_id
+        o[:image_name] = image.name
+        o[:arch] = image.arch
       end
-      field "created_at", s.created_at
-      field "deleted_at", s.deleted_at
-      field "private ips", s.interfaces.collect { |i| i['ipv4_address'] }.join(", ")
-      info ""
+      o[:private_ips] = s.interfaces.collect { |i| i['ipv4_address'] }.join(", ")
+      o[:cloud_ip_ids] = s.cloud_ips.collect { |i| i['id'] }.join(", ")
+      o[:cloud_ips] = s.cloud_ips.collect { |i| i['public_ip'] }.join(", ")
+      rows << o
     end
+
+    render_table(rows, { 
+                  :vertical => true, 
+                  :fields => [:id, :status, :name, :description, :created_at, :deleted_at, 
+                              :zone, :type, :type_name, :ram, :cores, 
+                              :disk, :image, :image_name, :private_ips, :cloud_ips, 
+                              :cloud_ip_ids
+                             ],
+                  :global => global_options})
 
   end
 end
