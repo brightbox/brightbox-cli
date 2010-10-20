15bf582d68faf307a52591db68c8014efad49d09
John Leach <john@johnleach.co.uk>
2010-10-20 13:25:56 +0100

    brightbox-servers: Remove 'show deleted servers' option, fixes #3043

--- lib/bbcloud/commands/servers-list.rb
+++ lib/bbcloud/commands/servers-list.rb
@@ -1,19 +1,9 @@
 desc 'List servers'
 arg_name '[server-id...]'
 command [:list] do |c|
-  c.desc "Show deleted servers"
-  c.switch [:d, "show-deleted"]
-
   c.action do |global_options,options,args|
 
-    servers = Server.find args
-
-    # Filter
-    servers.delete_if do |s|
-      next true if s.nil?
-      next true if !options[:d] and s.deleted?
-      false
-    end
+    servers = Server.find(args).compact
 
     render_table(servers, global_options)
   end
