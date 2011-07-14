4421d913381321186af55b85cab7591d1016d5bb
Hemant Kumar <gethemant@gmail.com>
2011-07-14 19:45:34 +0530

    expose ipv6 address from CLI tools

--- lib/bbcloud/commands/servers-show.rb
+++ lib/bbcloud/commands/servers-show.rb
@@ -27,7 +27,10 @@ module Brightbox
           o[:image_name] = s.image.name
           o[:arch] = s.image.arch
         end
-        o[:private_ips] = s.interfaces.collect { |i| i['ipv4_address'] }.join(", ")
+        interfaces = s.interfaces
+        o[:private_ips] = interfaces.collect { |i| i['ipv4_address'] }.join(", ")
+        o[:ipv6_address] = interfaces.collect { |i| i['ipv6_address'] }.join(", ")
+
         o[:cloud_ip_ids] = s.cloud_ips.collect { |i| i['id'] }.join(", ")
         o[:cloud_ips] = s.cloud_ips.collect { |i| i['public_ip'] }.join(", ")
         o[:snapshots] = s.snapshots.collect { |i| i['id'] }.join(", ")
@@ -38,7 +41,7 @@ module Brightbox
                                           :vertical => true,
                                           :fields => [:id, :status, :name, :description, :created_at, :deleted_at, 
                                             :zone, :type, :type_name, :type_handle, :ram, :cores, 
-                                            :disk, :image, :image_name, :private_ips, :cloud_ips, 
+                                            :disk, :image, :image_name, :private_ips, :cloud_ips, :ipv6_address,
                                             :cloud_ip_ids, :hostname, :public_hostname, :snapshots
                                           ]
                                         })
