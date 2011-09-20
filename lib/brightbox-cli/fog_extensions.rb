ebb3a5cbb1dd17565e40c81c59902e4209c90039
Caius Durling <caius@brightbox.co.uk>
2011-09-20 16:00:08 +0100

    Don't need to monkeypatch Fog::Connection for persistence anymore

--- lib/brightbox-cli/fog_extensions.rb
+++ lib/brightbox-cli/fog_extensions.rb
@@ -1,13 +1,3 @@
-# Hack to force persistent connections in fog
-module Fog
-  class Connection
-    def initialize(url, persistent=false)
-      @excon = Excon.new(url)
-      @persistent = true
-    end
-  end
-end
-
 Fog::Compute::Brightbox::Real.class_eval do |klass|
   klass.send(:attr_accessor, :oauth_token)
 end
