e4429115a1fbcf681a0af3d0bcfda554d101c43d
Caius Durling <caius@brightbox.co.uk>
2011-09-21 19:26:52 +0100

    Sorting out the cli's ServerGroup class

--- lib/brightbox-cli/server_groups.rb
+++ lib/brightbox-cli/server_groups.rb
@@ -1,13 +1,38 @@
 module Brightbox
   class ServerGroup < Api
 
+    def self.all
+      conn.server_groups
+    end
+
+    def self.get(id)
+      conn.server_groups.get(id)
+    end
+
     def self.create(options)
       new(conn.server_groups.create(options))
     end
 
+    def self.default_field_order
+      [:id, :name, :servers, :description]
+    end
+
+    def update(options)
+      self.class.conn.update_server_group(id, options)
+      self.reload
+      self
+    rescue Excon::Errors::BadRequest => e
+      raise Conflict, JSON.parse(e.response.body)['error']['details']
+    end
+
+    def destroy
+      fog_model.destroy
+    rescue Excon::Errors::Conflict => e
+      raise Conflict, "Cannot delete server group #{id}"
+    end
+
     def attributes
-      a = fog_model.attributes
-      a
+      fog_model.attributes
     end
 
     def to_row
@@ -18,24 +43,6 @@ module Brightbox
 
     def server_ids
       @server_ids ||= attributes["servers"].collect { |s| s["id"] } if attributes["servers"]
-    end
-
-    def destroy
-      fog_model.destroy
-    rescue Excon::Errors::Conflict => e
-      raise Conflict, "Cannot delete server group #{id}"
-    end
-
-    def self.get(id)
-      conn.server_groups.get(id)
-    end
-
-    def self.all
-      conn.server_groups
-    end
-
-    def self.default_field_order
-      [:id, :name, :servers, :description]
     end
 
   end
