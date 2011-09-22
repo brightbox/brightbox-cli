4dd9d658ae5173f76e4f3c89ae10f0f52e7def3f
Caius Durling <caius@brightbox.co.uk>
2011-09-22 09:48:22 +0100

    `groups remove_servers -a grp-id` removes all servers in the group
    Makes it easier to delete a group, `remove_servers -a`, then `destroy`.

--- lib/brightbox-cli/commands/groups-remove-servers.rb
+++ lib/brightbox-cli/commands/groups-remove-servers.rb
@@ -3,18 +3,30 @@ module Brightbox
   arg_name 'grp-id [srv-id...]'
   command [:remove_servers] do |c|
 
+    c.desc "Remove all servers from group"
+    c.switch [:a, :all]
+
     c.action do |global_options, options, args|
       grp_id = args.shift
-      raise "You must specify the server group and the server ids to remove" unless grp_id && grp_id[/^grp-/] && !args.empty?
+      raise "You must specify the server group and the server ids to remove" unless grp_id && grp_id[/^grp-/] && (!args.empty? || options[:a])
 
       sg = ServerGroup.find grp_id
 
-      servers = Server.find_or_call(args) do |id|
-        raise "Couldn't find server #{id}"
+      servers = if options[:a]
+        sg.server_ids
+      else
+        Server.find_or_call(args) do |id|
+          raise "Couldn't find server #{id}"
+        end
       end
 
-      info "Removing #{servers.size} servers from server group #{sg}"
-      sg.remove_servers servers
+      if servers.empty?
+        info "Server group #{sg} already contains zero servers"
+      else
+        info "Removing#{" all" if options[:a]} #{servers.size} servers from server group #{sg}"
+        sg.remove_servers servers
+      end
+
       sg.reload
       render_table([sg], global_options)
     end
