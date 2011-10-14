6db3ea2060aa98db85ac6ad44dc072a2d771960a
Hemant Kumar <gethemant@gmail.com>
2011-10-14 19:12:56 +0530

    Add unit tests for firewall rule show

--- spec/firewall_rule_spec.rb
+++ spec/firewall_rule_spec.rb
@@ -41,6 +41,34 @@ describe "FirewallRule" do
   end
 
   describe "Destroying firewall rule for a policy" do
+    use_vcr_cassette("firewall_rule_destroy")
+    before do
+      @firewall_rule = Brightbox::FirewallRule.find("fwr-tkklm")
+    end
 
+    it "should destroy a rule" do
+      lambda do
+        @firewall_rule.destroy
+      end.should_not raise_error
+    end
+  end
+
+  describe "Firewall rule show" do
+    use_vcr_cassette("firewall_rule_show")
+    before do
+      @firewall_rule = Brightbox::FirewallRule.find("fwr-ohc9w")
+      @display_options = { :fields => [
+          :id, :protocol,:source, :sport,:destination, :dport, :icmp_type, :firewall_policy
+        ],
+        :vertical => true
+      }
+    end
+    it "should display firewall rule" do
+      output = capture_stdout {
+        Brightbox.render_table([@firewall_rule],@display_options)
+      }
+      output.should match(/firewall_policy: fwp-30kea/)
+      output.should match(/dport: 22,80,443/)
+    end
   end
 end
