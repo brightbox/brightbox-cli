a6c0b6e393ca08526ad399b2f65a0b974044d0ed
Hemant Kumar <gethemant@gmail.com>
2011-10-24 16:53:40 +0530

    Fix problem with firewall list not working on 1.9

--- lib/brightbox-cli/commands/firewall-rules-list.rb
+++ lib/brightbox-cli/commands/firewall-rules-list.rb
@@ -3,14 +3,15 @@ module Brightbox
   arg_name '[firewall-policy-id...]'
   command [:list] do |c|
     c.action do |global_options,options,args|
-
       if args.empty?
-        raise "You must specify the firewall_policy_id as the first argument"
-      else
-        firewall_policy_id = args.shift
-        firewall_policy = FirewallPolicy.find_or_call(firewall_policy_id) do |id|
-          raise "Couldn't find firewall policy #{id}"
-        end
+        raise "You must specify the firewall policy id as the first argument"
+      end
+
+      firewall_policy_id = args.shift
+      raise "Invalid firewall policy id" unless firewall_policy_id =~ /^fwp-/
+
+      firewall_policy = FirewallPolicy.find_or_call([firewall_policy_id]) do |id|
+        raise "Couldn't find firewall policy #{id}"
       end
       render_table(FirewallRules.from_policy(firewall_policy.first), global_options)
     end
