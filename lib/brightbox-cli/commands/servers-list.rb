06898d899b5d8ceeb43d53564ce3b2f4e11d33a1
Caius Durling <dev@caius.name>
2011-11-22 19:55:03 +0000

    Allow listing servers in a specific group

--- lib/brightbox-cli/commands/servers-list.rb
+++ lib/brightbox-cli/commands/servers-list.rb
@@ -2,7 +2,14 @@ module Brightbox
   desc 'List servers'
   arg_name '[server-id...]'
   command [:list] do |c|
+
+    c.desc "Group identifier"
+    c.flag [:g, :group]
+
     c.action do |global_options,options,args|
+      # Check this here before we make any network connections
+      raise "A valid server group identifier is required for the group argument" unless options[:g] =~ /^grp-.{5}$/
+
       if args.empty?
         servers = Server.find(:all)
       else
@@ -10,6 +17,10 @@ module Brightbox
           warn "Couldn't find server #{id}"
         end
       end
+
+      # Scope by group if a group identifier is specified
+      servers = servers.select {|server| server.server_groups.any? {|grp| grp["id"] == options[:g] } } if options[:g]
+
       render_table(servers, global_options)
     end
   end
