dafa8240ceee6e7a0128ee60be3971eced32890d
Paul Thornthwaite <tokengeek@gmail.com>
2013-09-06 09:41:37 +0100

    Adds test for adding client with no config file
    Default test setup creates a skeleton config file but some bugs may
    exist when trying things related to the file itself. Hopefully this will
    catch them.

--- spec/commands/config/user_add_spec.rb
+++ spec/commands/config/user_add_spec.rb
@@ -26,6 +26,31 @@ describe "brightbox config" do
       end
     end
 
+    context "when NO config file on disk", :vcr do
+      let(:argv) { ["config", "user_add", email, client_id, secret] }
+      #let(:argv) { ["config", "user_add", email, client_id, secret, api_url] }
+
+      before do
+        config_file = File.join(ENV["HOME"], ".brightbox", "config")
+        FileUtils.rm_rf(config_file)
+        expect(File.exists?(config_file)).to be_false
+        mock_password_entry(password)
+      end
+
+      it "sets up the config" do
+        expect { output }.to_not raise_error
+
+        @config = Brightbox::BBConfig.new
+        @client_section = @config.config[client_id]
+
+        expect(@client_section["api_url"]).to eql("https://api.gb1.brightbox.com")
+        expect(@client_section["alias"]).to eql(client_id)
+        expect(@client_section["client_id"]).to eql(client_id)
+        expect(@client_section["secret"]).to eql(secret)
+        expect(@client_section["default_account"]).to eql(default_account)
+      end
+    end
+
     context "when passing in required arguments", :vcr do
       let(:argv) { ["config", "user_add", email, client_id, secret] }
       #let(:argv) { ["config", "user_add", email, client_id, secret, api_url] }
