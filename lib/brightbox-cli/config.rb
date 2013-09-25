0ef54d6536b164ac6e5570d99f1f635b44ec847b
Paul Thornthwaite <tokengeek@gmail.com>
2013-09-25 11:45:05 +0100

    Moves BBConfig client code into same module

--- lib/brightbox-cli/config.rb
+++ lib/brightbox-cli/config.rb
@@ -79,40 +79,6 @@ module Brightbox
       raise BBConfigError, "Config problem in #{config_filename}: #{e}"
     end
 
-    # Returns a client name or raises depending on a number of factors.
-    #
-    # FIXME This combines too much decision making into what appears to be a
-    # getter.
-    #
-    # If a default_client is not set and there are more than one client it
-    # raises an error because it is ambiguous which to use.
-    #
-    # If +force_default_config+ is passed in as false (for +config+ commands)
-    # then it attempts to use the first config but this becomes random on 1.8.7
-    #
-    # Calling +client_name+ within the +config+ command will break the config
-    # command because you can no longer manage your config (and set a default)
-    #
-    # This has been fixed by short circuiting to return +nil+ in that case which
-    # should never exist now.
-    #
-    def client_name
-      if @client_name
-        @client_name
-      else
-        # If we do not require a default client do not error if one is not set
-        # TODO Remove when certain unused
-        if @options[:force_default_config] == false
-          @client_name = default_client
-        else
-          # Is client ambigious?
-          if default_client.nil? && clients.length > 1
-            raise AmbiguousClientError, AMBIGUOUS_CLIENT_ERROR
-          end
-          @client_name = default_client || clients.first
-        end
-      end
-    end
 
     # Write out the configuration file to disk
     def save
@@ -120,38 +86,6 @@ module Brightbox
         config.write
         clean_up
       end
-    end
-
-    # @todo Account for "core" section
-    # @return [Boolean] +true+ if a client (section) has that name
-    def client_named?(name)
-      raw_sections.any? {|k,v| v["alias"] == name }
-    end
-
-    # @param [String] client_alias the name of the client to make the default
-    def set_default_client(client_alias)
-      dirty? unless client_alias == default_client
-      @default_client = client_alias
-      set_core_setting("default_client", client_alias)
-    end
-
-    # Returns the currently CONFIGURED default client (ignoring which client is
-    # asked for in this request)
-    #
-    # @return [String, NilClass] The client identifier or nil if not set
-    #
-    def default_client
-      @default_client ||= core_setting("default_client")
-    rescue
-      nil
-    end
-
-    def clear_default_client
-      if config["core"].has_key?("default_client")
-        config["core"].delete("default_client")
-        dirty!
-      end
-      @default_client = nil
     end
 
     # Outputs to debug the current values of the config/client's tokens
