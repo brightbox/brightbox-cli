3d5cef7bad61b0b95e2a6fa4dfe2aea0986fb7a8
Paul Thornthwaite <tokengeek@gmail.com>
2015-10-13 16:56:38 +0100

    Add spec for `BBConfig#clients`
    During some changes the behaviour broke and was not picked up directly
    by a spec but as a side issue for `SectionNameDeduplicator`.

---
+++ spec/unit/brightbox/bb_config/clients_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+describe Brightbox::BBConfig, "#clients" do
+  subject(:config) { config_from_contents(ini) }
+
+  context "when no sections exist" do
+    let(:ini) { "" }
+    it { expect(config.clients).to be_empty }
+  end
+
+  context "when only a 'core' section exists" do
+    let(:ini) do
+      <<-EOS
+      [core]
+      setting = value
+      EOS
+    end
+    it { expect(config.clients).to be_empty }
+  end
+
+  context "when multiple sections exist" do
+    let(:ini) do
+      <<-EOS
+      [core]
+      setting = value
+
+      [cli-12345]
+      setting = value
+
+      [app-12345]
+      setting = value
+      EOS
+    end
+    it { expect(config.clients).to eq(%w(cli-12345 app-12345)) }
+  end
+end
