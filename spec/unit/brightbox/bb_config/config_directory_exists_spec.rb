c74d002dfd11a0e2f8152f7f466f3cf950b085bb
Paul Thornthwaite <tokengeek@gmail.com>
2015-10-19 13:46:14 +0100

    Clean up spec for nonexistent config directory
    This fixes a recurring issue where running specs in random order can
    create a named config directory ahead of a spec testing if does not
    exist.
    
    This now creates a temp directory and immediately discards it before
    testing for it.
    
    Any directories created by spec errors will at least exist in tmp and
    should not clash.

--- spec/unit/brightbox/bb_config/config_directory_exists_spec.rb
+++ spec/unit/brightbox/bb_config/config_directory_exists_spec.rb
@@ -26,7 +26,8 @@ describe Brightbox::BBConfig do
 
     context "when directory does not exist" do
       it "returns false" do
-        @dir = "sdflfhdsafldsalfkjda"
+        # Create a tmpdir and delete it so we are sure it should not exist
+        Dir.mktmpdir { |dir| @dir = dir }
         expect(config.config_directory_exists?).to be false
       end
     end
