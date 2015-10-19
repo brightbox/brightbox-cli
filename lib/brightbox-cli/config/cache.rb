e916d0fea3511b2d65a2ff9163608be7a659316c
Paul Thornthwaite <tokengeek@gmail.com>
2015-10-19 13:50:32 +0100

    Fix caching attempts when directory missing
    Running specs in a random order could trigger a number of failures
    related to naughty API classes attempting to cache IDs whilst the
    directory used did not exist.
    
    This alters the `#cache_path` method to be read only without side effect
    and creates it when  `#cache_id` is called and it does not exist.

--- lib/brightbox-cli/config/cache.rb
+++ lib/brightbox-cli/config/cache.rb
@@ -2,21 +2,17 @@ module Brightbox
   module Config
     module Cache
       def cache_path
-        if @cache_path
-          @cache_path
-        else
-          @cache_path = File.join(config_directory, 'cache')
-          unless File.exist? @cache_path
-            begin
-              FileUtils.mkpath @cache_path
-            rescue Errno::EEXIST
-            end
-          end
-          @cache_path
-        end
+        File.join(config_directory, "cache")
       end
 
       def cache_id(cid)
+        return if cid.nil?
+        unless File.exist?(cache_path)
+          begin
+            FileUtils.mkpath(cache_path)
+          rescue Errno::EEXIST
+          end
+        end
         FileUtils.touch(File.join(cache_path, cid)) unless cid.nil?
       end
     end
