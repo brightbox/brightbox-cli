2c41950d259033f2efa02285962ec311a00a8522
Paul Thornthwaite <paul@brightbox.com>
2022-06-15 14:34:32 +0100

    Update `fog-brightbox` to `v1.5.0`
    This update includes support for volume resources but have not been
    added to the CLI yet.
    
    This also updates a number of VCR recordings that failed to replay to
    changes in the `fog-brightbox` implementation that resulted in missed
    matches due to changes in the request format.

--- brightbox-cli.gemspec
+++ brightbox-cli.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map { |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_dependency "fog-brightbox", ">= 1.3.0"
+  s.add_dependency "fog-brightbox", ">= 1.5.0"
   s.add_dependency "fog-core", "< 2.0"
   s.add_dependency "gli", "~> 2.21.0"
   s.add_dependency "i18n", ">= 0.6", "< 1.11"
--- spec/commands/sql/instances/create_spec.rb
+++ spec/commands/sql/instances/create_spec.rb
@@ -6,23 +6,30 @@ describe "brightbox sql instances" do
     let(:stdout) { output.stdout }
     let(:stderr) { output.stderr }
 
-    before do
-      config = config_from_contents(USER_APP_CONFIG_CONTENTS)
+    let(:sql_instance) { Brightbox::DatabaseServer.get(new_id) }
+    let(:new_id) { stdout.match(/id: (.*?)\n/)[1] }
 
-      # Setup in the VCR recordings
-      cache_access_token(config, "f83da712e6299cda953513ec07f7a754f747d727")
+    before do
+      config_from_contents(API_CLIENT_CONFIG_CONTENTS)
+      Brightbox.config.reauthenticate
+    end
+
+    after do
+      sql_instance.wait_for { ready? }
+      sql_instance.destroy
     end
 
     context "without arguments", vcr: true do
       let(:argv) { %w(sql instances create) }
-      let(:expected_password) { "477wwwj48yifrnuk" }
 
       it "reports the new admin username" do
-        expect(stdout).to include("admin_username: admin")
+        username = sql_instance.admin_username
+        expect(stdout).to include("admin_username: #{username}")
       end
 
       it "reports the new admin password" do
-        expect(stdout).to include("admin_password: #{expected_password}")
+        expected_password = sql_instance.admin_password
+        expect(stdout).to include("admin_password:")
       end
     end
 
@@ -32,17 +39,27 @@ describe "brightbox sql instances" do
 
       it "correctly sends API parameters" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
-        expect(stderr).to eql("")
+        expect(stderr).to eq("")
       end
     end
 
     context "--allow-access=srv-12345,grp-12345", vcr: true do
-      let(:argv) { %w(sql instances create --allow-access srv-12345,grp-12345) }
-      let(:expected_args) { { :allow_access => %w(srv-12345 grp-12345) } }
+      let(:argv) { ["sql", "instances", "create", "--allow-access", "#{server.id},#{group.id}"] }
+      let(:expected_args) { { :allow_access => [server.id, group.id] } }
+      let(:group) { Brightbox::ServerGroup.all.first }
+      let(:server) { Brightbox::Server.create flavor_id: "2gb.nbs", image_id: "img-linux" }
+
+      before do
+        server.fog_model.wait_for { ready? }
+      end
 
       it "correctly sends API parameters" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
-        expect(stderr).to eql("")
+        expect(stderr).to eq("")
+      end
+
+      after do
+        server.destroy
       end
     end
 
@@ -52,108 +69,74 @@ describe "brightbox sql instances" do
 
       it "correctly sends API parameters" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
-        expect(stderr).to eql("")
+        expect(stderr).to eq("")
+        expect(stdout).to include("version: 8.0")
       end
     end
 
-    context "--engine=mysql --engine-version=5.6", vcr: true do
-      let(:argv) { %w(sql instances create --engine=mysql --engine-version=5.6) }
-      let(:expected_args) { { :database_engine => "mysql", :database_version => "5.6" } }
+    context "--engine=mysql --engine-version=8.0", vcr: true do
+      let(:argv) { %w(sql instances create --engine=mysql --engine-version=8.0) }
+      let(:expected_args) { { :database_engine => "mysql", :database_version => "8.0" } }
 
       it "correctly sends API parameters" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
-        expect(stderr).to eql("")
+        expect(stderr).to eq("")
+        expect(stdout).to include("version: 8.0")
       end
     end
 
-    context "--maintenance-weekday=5 --maintenance_hour=11" do
+    context "--maintenance-weekday=5 --maintenance_hour=11", vcr: true do
       let(:argv) { %w(sql instances create --maintenance-weekday=5 --maintenance-hour=11) }
       let(:expected_args) { { :maintenance_weekday => "5", :maintenance_hour => "11" } }
 
-      before do
-        stub_request(:post, "http://api.brightbox.localhost/token").to_return(
-          :status => 200,
-          :body => '{"access_token":"44320b29286077c44f14c4efdfed70f63f4a8361","token_type":"Bearer","refresh_token":"759b2b28c228948a0ba5d07a89f39f9e268a95c0","scope":"infrastructure orbit","expires_in":7200}')
-
-        stub_request(:post, "http://api.brightbox.localhost/1.0/database_servers?account_id=acc-12345")
-          .with(:body => "{\"name\":null,\"description\":null,\"maintenance_weekday\":\"5\",\"maintenance_hour\":\"11\"}")
-      end
-
       it "correctly sends API parameters" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
-        expect(stderr).to eql("")
+        expect(stderr).to eq("")
       end
     end
 
-    context "--maintenance-weekday=thursday" do
+    context "--maintenance-weekday=thursday", vcr: true do
       let(:argv) { %w(sql instances create --maintenance-weekday=thursday) }
       let(:expected_args) { { :maintenance_weekday => "4" } }
 
-      before do
-        stub_request(:post, "http://api.brightbox.localhost/token").to_return(
-          :status => 200,
-          :body => '{"access_token":"44320b29286077c44f14c4efdfed70f63f4a8361","token_type":"Bearer","refresh_token":"759b2b28c228948a0ba5d07a89f39f9e268a95c0","scope":"infrastructure orbit","expires_in":7200}')
-
-        stub_request(:post, "http://api.brightbox.localhost/1.0/database_servers?account_id=acc-12345")
-          .with(:body => "{\"name\":null,\"description\":null,\"maintenance_weekday\":\"4\",\"maintenance_hour\":null}")
-      end
-
       it "correctly sends API parameters" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
-        expect(stderr).to eql("")
+        expect(stderr).to eq("")
       end
     end
 
-    context "--snapshot=dbi-1493j" do
-      let(:argv) { ["sql", "instances", "create", "--snapshot=dbi-1493j"] }
-      let(:expected_args) { { :snapshot_id => "dbi-1493j" } }
-
-      let(:json_response) do
-        <<-EOS
-        {
-          "id":"dbs-12345"
-        }
-        EOS
-      end
+    context "--snapshot=dbi-12345", vcr: true do
+      let(:argv) { ["sql", "instances", "create", "--snapshot=#{sql_snapshot.id}"] }
+      let(:expected_args) { { :snapshot_id => sql_snapshot.id } }
+      let(:source) { Brightbox::DatabaseServer.create({}) }
+      let(:sql_snapshot) { source.snapshot(true) }
 
       before do
-        stub_request(:post, "http://api.brightbox.localhost/1.0/database_servers?account_id=acc-12345")
-          .with(:headers => { "Content-Type" => "application/json" },
-                :body => hash_including("snapshot" => "dbi-1493j"))
-          .and_return(:status => 202, :body => json_response)
+        source.fog_model.wait_for { ready? }
+        sql_snapshot.wait_for { ready? }
+      end
+
+      after do
+        source.destroy
+        sql_snapshot.destroy
       end
 
       it "includes schedule fields in response" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
-        expect(stdout).to include("id: dbs-12345")
-        expect(stderr).to eql("")
+        expect(stdout).to include("id: #{sql_instance.id}")
+        expect(stderr).to eq("")
       end
     end
 
-    context "--snapshots-schedule='0 12 * * 4'" do
+    context "--snapshots-schedule='0 12 * * 4'", vcr: true do
       let(:argv) { ["sql", "instances", "create", "--snapshots-schedule=0 12 * * 4"] }
       let(:expected_args) { { :snapshots_schedule => "0 12 * * 4" } }
-
-      let(:json_response) do
-        <<-EOS
-        {
-          "id":"dbs-12345",
-          "snapshots_schedule":"0 12 * * 4",
-          "snapshots_schedule_next_at":"2016-07-07T12:00:00Z"
-        }
-        EOS
-      end
-
-      before do
-        stub_request(:post, "http://api.brightbox.localhost/1.0/database_servers?account_id=acc-12345")
-          .and_return(:status => 202, :body => json_response)
-      end
 
       it "includes schedule fields in response" do
         expect(Brightbox::DatabaseServer).to receive(:create).with(expected_args).and_call_original
         expect(stdout).to include("snapshots_schedule: 0 12 * * 4")
-        expect(stdout).to include("snapshots_schedule_next_at: 2016-07-07T12:00Z")
-        expect(stderr).to eql("")
+        expect(stdout).to match(/snapshots_schedule_next_at: 20\d{2}-\d{2}-\d{2}T12:\d{2}Z/)
+        expect(stderr).to eq("")
       end
     end
   end
