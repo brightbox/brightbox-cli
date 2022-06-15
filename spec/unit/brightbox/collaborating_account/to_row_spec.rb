6c9b48679b1a99f6097e51637ea78a937565573b
Paul Thornthwaite <paul@brightbox.com>
2022-06-15 11:39:08 +0100

    Replace deprecated `Fixnum` usage
    This is removed in later versions of Ruby and the specs fail due to the
    old reference to `Fixnum`. It is replaced with `Integer`.

--- spec/unit/brightbox/collaborating_account/to_row_spec.rb
+++ spec/unit/brightbox/collaborating_account/to_row_spec.rb
@@ -12,7 +12,7 @@ describe Brightbox::CollaboratingAccount do
 
       [:cloud_ips_limit, :lb_limit, :ram_limit, :ram_used, :ram_free].each do |key|
         it "returns the number for #{key}" do
-          expect(row_data[key]).to be_kind_of(Fixnum)
+          expect(row_data[key]).to be_kind_of(Integer)
         end
       end
     end
